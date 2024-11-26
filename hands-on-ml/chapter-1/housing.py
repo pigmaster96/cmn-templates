fe8bf073e5197b82471fed3d8c135840f85d65dc
pigmaster96 <malcolmtan2095@gmail.com>
2024-11-26 22:53:18 +0000

    random sampling

--- hands-on-ml/chapter-1/housing.py
+++ hands-on-ml/chapter-1/housing.py
@@ -3,7 +3,7 @@ import pandas as pd
 import tarfile
 import urllib.request
 
-#importing data
+##importing data
 def load_housing_data():
     tarball_path=Path("datasets/housing.tgz")
     #make directory if it doesn't exist
@@ -17,19 +17,22 @@ def load_housing_data():
 
 housing=load_housing_data()
 
-#wrangling
+##wrangling##
 
 #print(housing.info()) #non-null values for each category. see total_bedrooms is incomplete 
 #print(housing["ocean_proximity"].value_counts()) #this only takes a few values
 #print(housing.describe()) #this gives rough statistics of the features. 
 
-#now some plots
-import matplotlib.pyplot as plt #dependency for pandas .hist()
+#import matplotlib.pyplot as plt #dependency for pandas .hist()
 
-housing.hist(bins=50,figsize=(12,8))
-plt.show()
+#housing.hist(bins=50,figsize=(12,8))
+#plt.show() #note median income is in thousands, house median age and value are both capped
 
-
+##test set
+from sklearn.model_selection import train_test_split
+#we want a reproducible split of test/train set, so we set a seed(42)
+train_set,test_set=train_test_split(housing,test_size=0.2,random_state=42) #Split arrays or matrices into random train and test subsets. 
+#note that random sampling on small datasets may not be representative of the cohort! random sampling may not always be the way
 
 
 
