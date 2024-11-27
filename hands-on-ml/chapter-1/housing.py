8a04b9f6e319ff00f90295e63ba44b30dca8ae55
pigmaster96 <malcolmtan2095@gmail.com>
2024-11-27 19:50:00 +0000

    stratified sampling with pandas splitter

--- hands-on-ml/chapter-1/housing.py
+++ hands-on-ml/chapter-1/housing.py
@@ -17,22 +17,36 @@ def load_housing_data():
 
 housing=load_housing_data()
 
-##wrangling##
-
+###wrangling###
+import numpy as np
+#print(np.shape(housing)) #shape of data matrix
 #print(housing.info()) #non-null values for each category. see total_bedrooms is incomplete 
 #print(housing["ocean_proximity"].value_counts()) #this only takes a few values
 #print(housing.describe()) #this gives rough statistics of the features. 
 
-#import matplotlib.pyplot as plt #dependency for pandas .hist()
-
+import matplotlib.pyplot as plt #dependency for pandas .hist()
 #housing.hist(bins=50,figsize=(12,8))
 #plt.show() #note median income is in thousands, house median age and value are both capped
 
-##test set
-from sklearn.model_selection import train_test_split
-#we want a reproducible split of test/train set, so we set a seed(42)
-train_set,test_set=train_test_split(housing,test_size=0.2,random_state=42) #Split arrays or matrices into random train and test subsets. 
-#note that random sampling on small datasets may not be representative of the cohort! random sampling may not always be the way
+###splitting data###
+## randomly permuted test set:
+#from sklearn.model_selection import train_test_split
+##we want a reproducible split of test/train set, so we set a seed(42)
+#train_set,test_set=train_test_split(housing,test_size=0.2,random_state=42) #Split arrays or matrices into random train and test subsets.
+
+##split based on median income "category" so the test set is representative of popn "STRATIFIED SAMPLING" we set five categories here:
+housing["income_cat"]=pd.cut(housing["median_income"],
+                                     bins=[0.,1.5,3.0,4.5,6.,np.inf],
+                                     labels=[1,2,3,4,5]) #five bins. not including labels leaves an interval in the dataframe
+#print(housing.head())
+##bar graph:
+#print(housing["income_cat"])
+#print(housing["income_cat"].value_counts().sort_index())#first arrange by frequency, then list categories in order
+#housing["income_cat"].value_counts().sort_index().plot.bar(grid=True)
+#plt.xlabel("Income category")
+#plt.ylabel("Number of districts")
+#plt.show()
+
 
 
 
